ae9e97cd7dd7fef319dc6c516bcac3ce59b3489e
Chad Scira <chadvscira@gmail.com>
2016-12-19 09:08:43 +0700

    fixed isAsync for mangled async calls

--- dist/isAsync.js
+++ dist/isAsync.js
@@ -1,18 +1,33 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _stringSaw = require('string-saw');
+
+var _stringSaw2 = _interopRequireDefault(_stringSaw);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.default = function (func) {
-	var string = func.toString().trim();
+	var $string = (0, _stringSaw2.default)(func.toString()).trim();
 
 	return !!(
 	// native
-	string.match(/^async /) ||
+	$string.has(/^async /) ||
 
 	// babel
-	string.match(/return _ref[^\.]*\.apply/));
+	$string.has(/return _ref[^\.]*\.apply/) ||
+
+	// mangled
+	$string
+	// get inner function contents
+	.match(/^\s*[^\{]+\{([\s\S]+)\}\s*$/im).first()
+	// strip all sub calls
+	.replace(/\{[\s\S]+}/g, '')
+	// find return
+	.split('\n').trim().filter().has(/return .*promise/i));
 };
 
-module.exports = exports["default"];
+module.exports = exports['default'];
--- src/isAsync.js
+++ src/isAsync.js
@@ -1,11 +1,24 @@
+import saw from 'string-saw';
+
 export default func => {
-	const string = func.toString().trim();
+	const $string = saw(func.toString()).trim();
 
 	return !!(
 		// native
-		string.match(/^async /) ||
+		$string.has(/^async /) ||
 
 		// babel
-		string.match(/return _ref[^\.]*\.apply/)
+		$string.has(/return _ref[^\.]*\.apply/) ||
+
+		// mangled
+		$string
+		  // get inner function contents
+		  .match(/^\s*[^\{]+\{([\s\S]+)\}\s*$/im)
+		  .first()
+		  // strip all sub calls
+		  .replace(/\{[\s\S]+}/g,'')
+		  // find return
+		  .split('\n').trim().filter()
+		  .has(/return .*promise/i)
 	);
 };
